7b2d23061ce571cfb9dd39b87f82cf45d8098a0d
Keith Hultman <keithh@gmail.com>
2017-07-09 15:11:56 +0000

    dplyr replace na chain

--- 05-predict-new-tweet.R
+++ 05-predict-new-tweet.R
@@ -67,10 +67,8 @@ if(nrow(new_trump_tweet)>0) {
     inner_join(nrc_dummy) %>% 
     group_by(id) %>% 
     summarise_each(funs(max),starts_with("sentiment")) %>% 
-    right_join(new_trump_tweet, by = "id")
-  new_trump_sentiment[is.na(new_trump_sentiment)] <- 0
-
-  new_trump_sentiment <- new_trump_sentiment %>% 
+    right_join(new_trump_tweet, by = "id") %>% 
+    mutate_all(funs(replace(.,is.na(.), 0))) %>% 
     mutate_each(funs(convert_counts), starts_with("sentiment")) %>% 
     select(quote, picture, hashtag, dow, tod, starts_with("sentiment"), id, text)
   
